6a6cd26692d3b89f66ecae4fe6912ed7259bc1b7
Gary Russell <grussell@pivotal.io>
2017-05-29 14:21:26 -0400

    GH-319: Add Acknowledgment to Retry Context [1.x]
    Fixes #319
    
    Add the `Acknowledgment` to the retry context if the listener type warrants it.

--- spring-kafka/src/main/java/org/springframework/kafka/listener/adapter/RetryingMessageListenerAdapter.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/adapter/RetryingMessageListenerAdapter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +69,7 @@ public class RetryingMessageListenerAdapter<K, V>
 
 			@Override
 			public Void doWithRetry(RetryContext context) throws KafkaException {
-				context.setAttribute("record", record);
+				context.setAttribute(CONTEXT_RECORD, record);
 				RetryingMessageListenerAdapter.this.delegate.onMessage(record);
 				return null;
 			}
