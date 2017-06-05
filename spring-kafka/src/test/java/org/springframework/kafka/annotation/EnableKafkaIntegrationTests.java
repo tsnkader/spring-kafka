6856b21c1b8b5a425d0e45245e159200e5f635f2
Gary Russell <grussell@pivotal.io>
2017-06-05 09:34:14 -0400

    GH-335: Fix Class Cast with Batch Filter
    Fixes #335
    
    Failed to check `batchListener` field when casting listener to apply filter.
    
    Conflicts:
    	spring-kafka/src/main/java/org/springframework/kafka/config/AbstractKafkaListenerEndpoint.java
    	spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
    Resolved.

--- spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
@@ -120,10 +120,11 @@ public class EnableKafkaIntegrationTests {
 	public KafkaListenerEndpointRegistry registry;
 
 	@Autowired
-	private RecordFilterImpl recordFilter;
+	private RecordPassAllFilter recordFilter;
 
 	@Test
 	public void testSimple() throws Exception {
+		this.recordFilter.called = false;
 		template.send("annotated1", 0, "foo");
 		template.flush();
 		assertThat(this.listener.latch1.await(60, TimeUnit.SECONDS)).isTrue();
@@ -251,6 +252,7 @@ public class EnableKafkaIntegrationTests {
 	@Test
 	@SuppressWarnings("unchecked")
 	public void testBatch() throws Exception {
+		this.recordFilter.called = false;
 		ConcurrentMessageListenerContainer<?, ?> container =
 				(ConcurrentMessageListenerContainer<?, ?>) registry.getListenerContainer("list1");
 		Consumer<?, ?> consumer =
@@ -277,6 +279,7 @@ public class EnableKafkaIntegrationTests {
 		List<?> list = (List<?>) this.listener.payload;
 		assertThat(list.size()).isGreaterThan(0);
 		assertThat(list.get(0)).isInstanceOf(String.class);
+		assertThat(this.recordFilter.called).isTrue();
 
 		assertThat(commitLatch.await(10, TimeUnit.SECONDS)).isTrue();
 	}
@@ -388,13 +391,13 @@ public class EnableKafkaIntegrationTests {
 		}
 
 		@Bean
-		public RecordFilterImpl recordFilter() {
-			return new RecordFilterImpl();
+		public RecordPassAllFilter recordFilter() {
+			return new RecordPassAllFilter();
 		}
 
 		@Bean
-		public RecordFilterImpl manualFilter() {
-			return new RecordFilterImpl();
+		public RecordPassAllFilter manualFilter() {
+			return new RecordPassAllFilter();
 		}
 
 		@Bean
@@ -412,6 +415,7 @@ public class EnableKafkaIntegrationTests {
 					new ConcurrentKafkaListenerContainerFactory<>();
 			factory.setConsumerFactory(consumerFactory());
 			factory.setBatchListener(true);
+			factory.setRecordFilterStrategy(recordFilter());
 			return factory;
 		}
 
@@ -821,7 +825,7 @@ public class EnableKafkaIntegrationTests {
 
 	}
 
-	public static class RecordFilterImpl implements RecordFilterStrategy<Integer, String> {
+	public static class RecordPassAllFilter implements RecordFilterStrategy<Integer, String> {
 
 		private boolean called;
 
